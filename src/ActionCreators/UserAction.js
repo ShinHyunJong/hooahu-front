dc648f3523f67c104e761bbfa5ad0ac6414a725f
서그림 <seogeurim@seogeulim-ui-MacBook-Pro.local>
2018-08-04 18:36:05 +0900

    Landing Page

--- src/ActionCreators/UserAction.js
+++ src/ActionCreators/UserAction.js
@@ -3,6 +3,9 @@ import { ServerEndPoint } from "../Configs/Server";
 export const SUCCEED_TO_GET_USER = "SUCCEED_TO_GET_USER";
 export const FAILED_TO_GET_USER = "FAILED_TO_GET_USER";
 
+export const SUCCEED_TO_CHECKNAME = "SUCCEED_TO_CHECKNAME";
+export const FAILED_TO_CHECKNAME = "FAILED_TO_CHECKNAME";
+
 export const getUser = token => {
   return async dispatch => {
     try {
@@ -23,6 +26,34 @@ export const getUser = token => {
     } catch (error) {
       dispatch({
         type: FAILED_TO_GET_USER,
+        payload: { data: "NETWORK_ERROR" }
+      });
+    }
+  };
+};
+
+export const checkEmail = params => {
+  return async dispatch => {
+    try {
+      let response = await fetch(
+        ServerEndPoint + `api/auth/email/test?email=${params.email}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      let responseJson = await response.json();
+      await dispatch({
+        type: SUCCEED_TO_CHECKNAME,
+        payload: responseJson
+      });
+      return responseJson;
+    } catch (error) {
+      dispatch({
+        type: FAILED_TO_CHECKNAME,
         payload: { data: "NETWORK_ERROR" }
       });
     }
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -8,8 +8,10 @@ import filterJson from "../../Json/filter";
 import ec from "../../Json/ec";
 import cx from "classnames";
 import NumberFormat from "react-number-format";
+import * as UserAction from "../../ActionCreators/UserAction";
 import { NavBar, BoxList, Post, Thumb, SocialInput } from "../../Components";
 import ProgressiveImage from "react-progressive-image-loading";
+import { withRouter } from "react-router-dom"; // Material UI Provider for React
 
 const defaultProps = {};
 const propTypes = {};
@@ -23,21 +25,91 @@ const mapStateToProps = state => {
 class LandingPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: "",
+      password: "",
+      isChecking: false,
+      isValid: true,
+      isLength: false
+    };
   }
 
   render() {
+    const { isChecking, isValid, isLength } = this.state;
     return (
       <div className="landingPage">
-        <NavBar isActive="feed" listClassName="landingPage__tabBar__list" />
         <div className="landingPage__welcome">
-          <h2>로그인 안했을 때</h2>
+          <div className="landingPage__welcome__info">
+            <h1>
+              Join and get connected <br />
+              with people in <b>USFK Community</b>
+            </h1>
+            <h2>
+              If you are new to Hooah!U,{" "}
+              <span
+                className="landingPage__welcome__info-signUp"
+                onClick={this.handleSignUp}
+              >
+                Sign up
+              </span>{" "}
+              to join the network.
+            </h2>
+          </div>
+          <div className="landingPage__welcome__input">
+            <input
+              className="landingPage__welcome__input__blank"
+              placeholder="아이디"
+              onChange={this.handleInput}
+            />
+            <input
+              className="landingPage__welcome__input__blank"
+              placeholder="비밀번호"
+              onChange={this.handlePassword}
+            />
+            {isChecking ? <h1>체킹중</h1> : null}
+            {!isValid ? (
+              <h1>이미 가입된 이메일입니다.</h1>
+            ) : (
+              <h1>가능합니다!</h1>
+            )}
+            {isLength ? <h1>유효합니다.</h1> : <h1>유효하지 않음</h1>}
+            <button>Login</button>
+          </div>
         </div>
+        <div className="landingPage__about" />
       </div>
     );
   }
+
+  handleInput = e => {
+    this.setState({ email: e.target.value, isChecking: true });
+    const { dispatch } = this.props;
+    const params = { email: e.target.value };
+    dispatch(UserAction.checkEmail(params)).then(value => {
+      if (value.isExists) {
+        this.setState({ isChecking: false, isValid: false });
+      } else {
+        this.setState({ isChecking: false, isValid: true });
+      }
+    });
+  };
+
+  handlePassword = e => {
+    if (e.target.value.length > 6) {
+      this.setState({ isLength: true });
+    } else {
+      this.setState({ isLength: false });
+    }
+  };
+
+  handleSignUp = () => {
+    this.props.history.push({
+      pathname: "/signup"
+    });
+  };
 }
 
 LandingPage.defaultProps = defaultProps;
 LandingPage.propTypes = propTypes;
 
-export default connect(mapStateToProps)(LandingPage);
+export default withRouter(connect(mapStateToProps)(LandingPage));
